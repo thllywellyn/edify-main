56737c8a0d319b5efeed54fd3326be36ec7c70e7
LatikaBansal08 <122596002+LatikaBansal08@users.noreply.github.com>
2025-03-03 17:15:42 +0530

    progress

--- server/models/CourseProgress.js
+++ server/models/CourseProgress.js
@@ -1,20 +1,9 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const courseProgress = new mongoose.Schema({
-  courseID: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Course",
-  },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
-  },
-  completedVideos: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "SubSection",
-    },
-  ],
-})
+const courseProgressSchema = new mongoose.Schema({
+    courseID: { type: mongoose.Schema.Types.ObjectId, ref: "Course" },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    completedVideos: [{ type: mongoose.Schema.Types.ObjectId, ref: "SubSection" }],
+});
 
-module.exports = mongoose.model("courseProgress", courseProgress)
+module.exports = mongoose.model("CourseProgress", courseProgressSchema);
