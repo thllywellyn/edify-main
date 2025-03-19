63264d92eb2cfc36d4af5f42b7088425de95bd11
Llywellyn Sana <71801906+thllywellyn@users.noreply.github.com>
2025-03-19 12:13:13 +0000

    cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,26 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import db from './database/db.js';
-import {app} from './app.js'
+import { app } from './app.js';
+import cors from 'cors';
+
+app.use(cors({
+  origin: '*', // Allows all origins
+  credentials: false // Credentials disabled
+}));
+console.log("cors done")
+
 dotenv.config({
-    path: './.env'
-})
+  path: './.env'
+});
 
 console.log(`${process.env.DB_NAME}`);
 
-
 db()
-.then(() => {
+  .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
-    console.log(" mongodb connection failed !!! ", err);
-})
+      console.log(`⚙️ Server is running at port : ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongodb connection failed !!!", err);
+  });
