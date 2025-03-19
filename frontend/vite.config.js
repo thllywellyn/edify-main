1696becfdad30a156e440e11f21b1338cab34e54
Llywellyn Sana <71801906+thllywellyn@users.noreply.github.com>
2025-03-19 11:52:53 +0000

    vite config changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 4000,
-    proxy: {
-      '/api': 'https://edifyserv.lsanalab.xyz'
-    }
+    port: process.env.PORT || 4000,
+    host: true,
   },
   plugins: [react()],
 })
