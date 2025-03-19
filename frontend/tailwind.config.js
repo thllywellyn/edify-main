9b5d14f9f7e3a0d46d7868d53d6d308de919d4ae
Llywellyn Sana <71801906+thllywellyn@users.noreply.github.com>
2025-03-19 11:44:26 +0000

    require changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,4 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
+import withMT from "@material-tailwind/react/utils/withMT";
  
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
