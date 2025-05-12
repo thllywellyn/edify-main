fa075614a31eede8ea180fe471c39c3a8968282b
Llywellyn Sana <thllywellyn@gmail.com>
2025-05-12 17:14:12 +0530

    react-slick

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -20,10 +20,29 @@ export default defineConfig({
     allowedHosts: ['edify.lsanalab.xyz']
   },
   plugins: [
-    react(),
-    VitePWA({
+    react(),    VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,wasm}'],
+        globDirectory: 'dist',
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      },
       manifest: {
         name: 'Edify',
         short_name: 'Edify',
@@ -49,18 +68,18 @@ export default defineConfig({
         orientation: 'portrait'
       }
     })
-  ],
-  build: {
+  ],  build: {
     outDir: 'dist',
     emptyOutDir: true,
     chunkSizeWarningLimit: 1000,
     sourcemap: true,
     minify: 'esbuild',
     rollupOptions: {
+      external: ['react-slick'],
       output: {
         manualChunks: {
           'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'ui-vendor': ['@material-tailwind/react', 'react-hot-toast', 'react-icons'],
+          'ui-vendor': ['@material-tailwind/react', 'react-hot-toast', 'react-icons', 'react-slick'],
           'utils-vendor': ['axios', 'date-fns']
         }
       }
